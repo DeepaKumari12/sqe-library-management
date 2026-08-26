55a14066b84eb79818eb21caff4ad54c570e0247
nDeepaKumari12 <deepa.parhyar12@gmail.com>
2026-08-26 16:33:59 +0500

    chore: initial project structure

---
+++ src/library/book.py
@@ -0,0 +1,7 @@
+class Book:
+    """Minimal Book class stub for the Library Management System."""
+
+    def __init__(self, title, book_id, status="available"):
+        self.title = title
+        self.book_id = book_id
+        self.status = status
